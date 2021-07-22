3d5a9e0a028e03a3decfb791ca64e575536565c2
TatianaShapoval <questopunto@gmail.com>
2021-07-23 02:24:58 +0300

    adaptive 3 blocks

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 $(function(){
-    var mixer = mixitup('.portfolio__content');
-
+    
     $('.slider-blog__inner').slick({
         dots: true,
         prevArrow:'<button type="button" class="slick-prev"><img src="images/Shapeleft.svg" alt=""></button>',
         nextArrow:'<button type="button" class="slick-next"><img src="images/Shaperight.svg" alt=""></button>'
 
     });
+
+    $('.menu__btn').on('click', function(){
+        $('.header__top-inner').toggleClass('header__top-inner--active');
+    
+    });
+
+    var mixer = mixitup('.portfolio__content');
+
 
 });
