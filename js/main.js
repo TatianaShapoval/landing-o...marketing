a3f0bad7b64efdeb9d2a4f09fddb523b9135b538
TatianaShapoval <questopunto@gmail.com>
2021-07-24 04:19:52 +0300

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,27 +1,23 @@
-$(function(){
-    
-    $('.slider-blog__inner').slick({
-        dots: true,
-        prevArrow:'<button type="button" class="slick-prev"><img src="images/Shapeleft.svg" alt=""></button>',
-        nextArrow:'<button type="button" class="slick-next"><img src="images/Shaperight.svg" alt=""></button>',
-        responsive: [
-            {
-              breakpoint: 769,
-              settings: {
-                arrows: false
-              }
-            },
-          ]
-    
+$(function () {
+  $(".slider-blog__inner").slick({
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/Shapeleft.svg" alt=""></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/Shaperight.svg" alt=""></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
 
-    });
+  $(".menu__btn").on("click", function () {
+    $(".header__top-inner").toggleClass("header__top-inner--active");
+  });
 
-    $('.menu__btn').on('click', function(){
-        $('.header__top-inner').toggleClass('header__top-inner--active');
-    
-    });
-
-    var mixer = mixitup('.portfolio__content');
-
-
+  var mixer = mixitup(".portfolio__content");
 });
