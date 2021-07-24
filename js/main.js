d128e33f2ec2a0cb2cf2e24ec75c8653e289b2c0
TatianaShapoval <questopunto@gmail.com>
2021-07-24 03:46:52 +0300

    completed responcive layout

--- js/main.js
+++ js/main.js
@@ -3,7 +3,16 @@ $(function(){
     $('.slider-blog__inner').slick({
         dots: true,
         prevArrow:'<button type="button" class="slick-prev"><img src="images/Shapeleft.svg" alt=""></button>',
-        nextArrow:'<button type="button" class="slick-next"><img src="images/Shaperight.svg" alt=""></button>'
+        nextArrow:'<button type="button" class="slick-next"><img src="images/Shaperight.svg" alt=""></button>',
+        responsive: [
+            {
+              breakpoint: 769,
+              settings: {
+                arrows: false
+              }
+            },
+          ]
+    
 
     });
 
